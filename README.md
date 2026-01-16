25a660ca60e0597784dea4b7d76a34b0f02bc8ce
Ibrahim Baatiah <62605376+Ibrahim2122@users.noreply.github.com>
2026-01-16 22:29:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 # programming-languages-for-cloud-cystems
+=======
+# Programming-Languages-for-Cloud-Systems
+Course labs for Cloud Programming
+>>>>>>> 892b188 (Initial commit)
---
+++ javascript/stage1/s1-var-types.js
@@ -0,0 +1,20 @@
+var a = "Hello, World!";
+
+let b = 123;
+
+const c = true;
+
+console.table([
+  {
+    type: typeof a,
+    val: a,
+  },
+  {
+    type: typeof b,
+    val: b,
+  },
+  {
+    type: typeof c,
+    val: c,
+  },
+]);
