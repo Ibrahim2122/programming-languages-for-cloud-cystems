b84186a5261dadaf23aa6ffcbb2bf63d61278d81
Ibrahim Omar <abra56heem@gmail.com>
2026-01-21 20:35:21 +0100

    task25: s2_for_01 - s2_for_01

---
+++ stage2/B/S2_FOR_01.py
@@ -0,0 +1,17 @@
+# S2_FOR_01.py
+
+def main():
+    N = 30
+    for i in range(1, N + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    main()
