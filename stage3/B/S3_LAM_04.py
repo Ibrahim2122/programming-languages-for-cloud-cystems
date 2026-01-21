623af6414f5c5026f8b997322ed28ba15da2f873
Ibrahim Omar <abra56heem@gmail.com>
2026-01-21 23:19:13 +0100

    task40: s3_pipe_04 - s3_pipe_04

---
+++ stage3/B/S3_LAM_04.py
@@ -0,0 +1,19 @@
+# S3_LAM_04.py
+
+from functools import reduce
+
+def main():
+    nums = [1, 2, 3, 4, 5, 6]
+
+    evens = list(filter(lambda n: n % 2 == 0, nums))
+    squares = list(map(lambda n: n * n, evens))
+    total = reduce(lambda a, b: a + b, squares, 0)
+
+    print("nums:   ", nums)
+    print("evens:  ", evens)
+    print("squares:", squares)
+    print("sum:    ", total)
+
+
+if __name__ == "__main__":
+    main()
